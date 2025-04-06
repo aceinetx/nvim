ba15d4034a120fcd399b9b1cedc13984b977f4b7
aceinetx <mouselin82@gmail.com>
2025-04-06 16:55:35 +0300

    Properly disable arrow keys

--- init.lua
+++ init.lua
@@ -90,11 +90,6 @@ vim.cmd [[
 	set tabstop=2
 	set shiftwidth=2
 
-	map <Up> <Nop>
-	map <Left> <Nop>
-	map <Right> <Nop>
-	map <Down> <Nop>
-
 	highlight CursorLine cterm=NONE ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE
 	set cursorline
 ]]
@@ -128,3 +123,13 @@ cmp.setup({
 		{ name = "luasnip" },
 	},
 })
+
+vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', {noremap = true, silent = true})
+
+vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<Right>', '<Nop>', {noremap = true, silent = true})
