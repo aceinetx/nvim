f0a317b2ad13de975d02632ae88852746f54f14d
aceinetx <mouselin82@gmail.com>
2025-04-06 17:08:33 +0300

    Disable arrow keys for visual mode too

--- init.lua
+++ init.lua
@@ -129,6 +129,11 @@ vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', {noremap = true, silent = true})
 
+vim.api.nvim_set_keymap('v', '<Up>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<Down>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<Left>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<Right>', '<Nop>', {noremap = true, silent = true})
+
 vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', {noremap = true, silent = true})
