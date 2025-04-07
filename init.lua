fd6199a31414bee7c8e99e33e1a1207b34a43986
aceinetx <mouselin82@gmail.com>
2025-04-07 13:23:14 +0300

    Add an option to toggle arrow keys

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+arrow_keys = false -- I usually enable this when i'm in termux, i wouldn't recommend you enabling it tho
+
 vim.opt.runtimepath:prepend("~/.config/nvim/lua/lazy")
 local lspconfig = require('lspconfig')
 lspconfig.clangd.setup{}
@@ -127,17 +129,19 @@ cmp.setup({
 	},
 })
 
-vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', {noremap = true, silent = true})
+if not arrow_keys then
+	vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap('v', '<Up>', '<Nop>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '<Down>', '<Nop>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '<Left>', '<Nop>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '<Right>', '<Nop>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('v', '<Up>', '<Nop>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('v', '<Down>', '<Nop>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('v', '<Left>', '<Nop>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('v', '<Right>', '<Nop>', {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', '<Right>', '<Nop>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('i', '<Right>', '<Nop>', {noremap = true, silent = true})
+end
