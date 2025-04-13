fc0a64462469080c2dd43f66708edd8e4e69b910
aceinetx <mouselin82@gmail.com>
2025-04-13 21:43:32 +0300

    Fix macros incase where there is leading characters at the end of a line

--- lua/macros.lua
+++ lua/macros.lua
@@ -1,4 +1,4 @@
 local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
 
-vim.fn.setreg("o", "0f<xi\"" .. esc .. "f>xa\"" .. esc .. "0")
-vim.fn.setreg("p", "0f\"xi<" .. esc .. "f\"xa>" .. esc .. "0")
+vim.fn.setreg("o", "0f<xi\"" .. esc .. "$a " .. esc .. "0f>xi\"" .. esc .. "$x0")
+vim.fn.setreg("p", "0f\"xi<" .. esc .. "$a " .. esc .. "0f\"xi>" .. esc .. "$x0")
