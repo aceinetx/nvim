61c45e0a982ad60ec5472bae6b400b23e64d54b0
aceinetx <mouselin82@gmail.com>
2025-07-02 18:54:18 +0000

    Fix inline diagnostics messages

--- lua/lsp_cfg.lua
+++ lua/lsp_cfg.lua
@@ -6,3 +6,7 @@ lspconfig.rust_analyzer.setup{}
 lspconfig.omnisharp.setup{
 	cmd = { "dotnet", "/usr/lib/omnisharp-roslyn/OmniSharp.dll" },
 }
+vim.diagnostic.enable = true
+vim.diagnostic.config({
+	virtual_lines = true,
+})
