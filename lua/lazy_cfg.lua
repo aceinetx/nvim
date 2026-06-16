8eaca3afc79ead4b65ded7a73d3afa845847d4be
aceinetx <mouselin82@gmail.com>
2026-06-16 14:28:19 +0000

    remove tiny glimmer

--- lua/lazy_cfg.lua
+++ lua/lazy_cfg.lua
@@ -38,13 +38,6 @@ require('lazy').setup({
 		opts = {}
 	},
 	{
-		"rachartier/tiny-glimmer.nvim",
-		event = "VeryLazy",
-		priority = 10, -- Needs to be a really low priority, to catch others plugins keybindings.
-		opts = {
-		},
-	},
-	{
 		{'akinsho/toggleterm.nvim', version = "*", 
 			opts = {
 				open_mapping = [[<A-t>]],
